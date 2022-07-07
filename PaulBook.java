2861d29720094b7784f2a6b5d55220096e1cce7d
AnuAmruta1 <108450343+AnuAmruta1@users.noreply.github.com>
2022-07-07 13:28:16 +0530

    Add files via upload

---
+++ PaulBook.java
@@ -0,0 +1,17 @@
+class PaulBook
+{
+	public static void main(String[] args)
+	{
+		String name="paul";
+		String brand="idol";
+		double processorspeedInGHz=2.66;
+		double ramSizeInGB=8;
+		int HardDiskInGB=500;
+		int moniterInInch=15;
+		double price=1760.55;
+		char servicePlan='C';
+		boolean onSiteService=true;
+		boolean ExetendedWarranty=false;
+				
+}
+}

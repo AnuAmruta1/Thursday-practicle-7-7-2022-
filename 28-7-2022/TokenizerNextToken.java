91f00ff34ae9d36016fa6b7f7344f2be46a78296
AnuAmruta1 <108450343+AnuAmruta1@users.noreply.github.com>
2022-07-29 10:51:46 +0530

    Delete TokenizerNextToken.java

--- 28-7-2022/TokenizerNextToken.java
+++
@@ -1,9 +0,0 @@
-import java.util.StringTokenizer;  
-class TokenizerNextToken
-{  
-	public static void main(String[] args) 
-	{  
-		StringTokenizer st = new StringTokenizer("my,name,is,Amruta");    
-		System.out.println("Next token is : " + st.nextToken(","));  
-  	}      
-}  

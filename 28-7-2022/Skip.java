0c4854bc5ffa64bacf8d716b2b59edf51c64d346
AnuAmruta1 <108450343+AnuAmruta1@users.noreply.github.com>
2022-07-29 12:32:17 +0530

    Add files via upload

---
+++ 28-7-2022/Skip.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+class Skip
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			FileInputStream f=new FileInputStream("test-file5.txt");
+			int i=0;
+			f.skip(1);
+			System.out.println("Priting text from index 5:");
+			while((i=f.read())!=-1)
+			{
+				System.out.print((char)i);
+			}
+			f.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
